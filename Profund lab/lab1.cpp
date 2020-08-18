17363b436e24f0c39aa87a64c13e65912db215c1
PEERAPAT SATTAPORNNARA <63010702@KMITL.AC.TH>
2020-08-18 22:53:15 +0700

    Add project files.

---
+++ Profund lab/lab1.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int A, B = 2;
+	printf("Enter number : ");
+	scanf("%d", &A);
+	printf("Factoring reslut : ");
+	while (B < A && B >= 2)
+	{
+		if (A % B == 0)
+		{
+			printf("%d x ", B);
+			A = A / B;
+		}
+		else B = B + 1;
+
+	}
+	if (B = A)
+	{
+		printf(" %d ", B);
+	}
+}
